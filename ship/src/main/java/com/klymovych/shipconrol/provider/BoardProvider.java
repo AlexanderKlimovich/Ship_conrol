267d7f415e2f996669147a5be6fe4edd23ac4282
AlexanderKlimovich <alexanderklimovich1@gmail.com>
2023-08-23 16:23:21 +0300

    make BoardProvider for adding boards to app from application.yaml

---
+++ ship/src/main/java/com/klymovych/shipconrol/provider/BoardProvider.java
@@ -0,0 +1,20 @@
+package com.klymovych.shipconrol.provider;
+
+import com.klymovych.shipconrol.common.bean.Board;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Getter
+@Component
+@ConfigurationProperties(prefix = "application")
+public class BoardProvider {
+
+    private final List<Board> boards = new ArrayList<>();
+}
